de90b9f19cc0cd017a4459a77ae4952c74106865
Clement <crymycm@gmail.com>
2024-12-20 02:20:16 +0100

    fix authorize requests

--- app/Http/Requests/CuveIndexRequest.php
+++ app/Http/Requests/CuveIndexRequest.php
@@ -11,7 +11,7 @@ class CuveIndexRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/ProprietaireIndexRequest.php
+++ app/Http/Requests/ProprietaireIndexRequest.php
@@ -11,7 +11,7 @@ class ProprietaireIndexRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
